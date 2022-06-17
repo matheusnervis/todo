ed681813c91392539899b74aadbdfe48ac48d68e
Matheus Nervis da Silva <matheusnervis@outlook.com>
2022-06-17 19:01:16 -0400

    Improved interface - Update list of tasks - Add form to add task - Add functions to add and remove tasks

--- app/static/script.js
+++ app/static/script.js
@@ -2,11 +2,38 @@ const app = Vue.createApp({
     data() {
         return {
             title: "TODO",
+            idNext: 4,
             tasks: [
                 {id: 1, description: "Do Something", done: false},
                 {id: 2, description: "Do other Something", done: true},
                 {id: 3, description: "Do more other Something", done: false}
-            ]
+            ],
+            taskDescription: '',
+        }
+    },
+    methods: {
+        addTask() {
+            if (this.taskDescription == '') {
+                return
+            }
+
+            let task = {
+                id: this.idNext,
+                description: this.taskDescription,
+                done: false
+            }
+            this.tasks.push(task)
+            this.idNext++
+
+            this.taskDescription= ""
+        },
+        deleteTask(id) {
+            for (let i=0; i<this.tasks.length; i++){
+                if (this.tasks[i].id == id){
+                    this.tasks.splice(i, 1)
+                    break
+                }
+            }
         }
     }
 })

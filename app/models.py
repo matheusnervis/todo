bb014737b6ab1a76a0131a68ac444ce8f08be234
Matheus Nervis da Silva <matheusnervis@outlook.com>
2022-11-08 16:13:14 -0400

    Add Models and Schemas for Tasks

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Boolean, Column, Integer, String
+
+from .db import Base
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True)
+    # name = Column(String(50), nullable=False)
+    description = Column(String(250), nullable=False)
+    done = Column(Boolean, default=True)
---
+++ app/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel
+
+
+class TaskCreate(BaseModel):
+    description: str
+
+
+class Task(TaskCreate):
+    id: int
+    done: bool
+
+    class Config:
+        orm_mode = True
+
+
+class TaskList(BaseModel):
+    tasks: list[Task]
